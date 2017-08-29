7a44540a5596e4dcd3447fbaae3c07078cfec368
name <carobox96@gmail.com>
2017-08-29 14:02:26 -0600

    PracticaU1_1_LayoutsymasLayout_AnaCarolinaMondragonRangel 29/08/2017

---
+++ PracticaU1_1_LayoutsymasLayout_AnaCarolinaMondragonRangel/app/src/main/java/mx/edu/ittepic/practicau1_1_layoutsymaslayout_anacarolinamondragonrangel/pantalla2.java
@@ -0,0 +1,34 @@
+package mx.edu.ittepic.practicau1_1_layoutsymaslayout_anacarolinamondragonrangel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla2);
+    }
+    public void ventana(View vista){
+        Intent intent = new Intent(this, pantalla3.class);
+        startActivity(intent);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
