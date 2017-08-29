03e919475e7fc28bc4034d242058f18dc4441453
name <carobox96@gmail.com>
2017-08-29 14:44:32 -0600

    Actividad en clase dia 29/08/2017

---
+++ DADM_U1_Ejercicio1/app/src/main/java/mx/edu/ittepic/dadm_u1_ejercicio1/Ecuacion.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.dadm_u1_ejercicio1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ecuacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecuacion);
+    }
+}
