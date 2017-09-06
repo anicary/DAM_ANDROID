027d08283eac8fa6ca6319edaaa37765ddf23e7c
ancamondragon <carobox96@gmail.com>
2017-09-06 11:06:06 -0600

    ejercicio aeropuerto

---
+++ DADM_U1_Ejercicio6/app/src/main/java/mx/edu/ittepic/dadm_u1_ejercicio6/TableLayout1.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.dadm_u1_ejercicio6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableLayout1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layout1);
+    }
+}
