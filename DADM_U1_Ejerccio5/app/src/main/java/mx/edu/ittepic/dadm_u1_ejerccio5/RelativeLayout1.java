d00dc96c3f34aba53e4820d44f75707bfb053463
ancamondragon <carobox96@gmail.com>
2017-09-05 22:55:35 -0600

    ejercicio CONVERTIDOR DOLAR/EURO

---
+++ DADM_U1_Ejerccio5/app/src/main/java/mx/edu/ittepic/dadm_u1_ejerccio5/RelativeLayout1.java
@@ -0,0 +1,70 @@
+package mx.edu.ittepic.dadm_u1_ejerccio5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RelativeLayout1 extends AppCompatActivity {
+    int Pdolar, Peuro, Pprecio;
+    EditText dolar, euro, precio;
+    Button convertir, asignar;
+    TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout1);
+
+        convertir = (Button) findViewById(R.id.btn1);
+        asignar = (Button) findViewById(R.id.btn2);
+
+        dolar = (EditText) findViewById(R.id.canditaddolares);
+        euro = (EditText) findViewById(R.id.cantidadeuros);
+        precio = (EditText) findViewById(R.id.cantidadprecio);
+
+        resultado = (TextView) findViewById(R.id.resultado);
+
+    }
+
+    public void asignar(View v) {
+        try {
+            Pprecio = Integer.parseInt(precio.getText().toString());
+
+        } catch (NumberFormatException e) {
+        }
+    }
+
+    public void convertir(View v) {
+
+        if (Pprecio != 0) {
+
+            if (dolar.getText().toString().trim().length() == 0) {
+
+                try {
+
+                    Peuro = Integer.parseInt(euro.getText().toString());
+                    resultado.setText("Resultado:" + ((Pprecio * Peuro) * 1.20));
+
+                } catch (NumberFormatException e) {
+                    Toast.makeText(this, "error1", Toast.LENGTH_SHORT).show();
+                }
+
+            } else {
+                try {
+                    Pdolar = Integer.parseInt(dolar.getText().toString());
+                    resultado.setText("Resultado:" + (Pprecio * Pdolar));
+                } catch (NumberFormatException e) {
+                    Toast.makeText(this, "error2", Toast.LENGTH_SHORT).show();
+                }
+            }
+            } else{
+                Toast.makeText(this, "error no se asigno ningun valor al peso", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
---
+++ DADM_U1_Ejercicio5/app/src/main/java/mx/edu/ittepic/dadm_u1_ejercicio5/RelativeLayout.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.dadm_u1_ejercicio5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
