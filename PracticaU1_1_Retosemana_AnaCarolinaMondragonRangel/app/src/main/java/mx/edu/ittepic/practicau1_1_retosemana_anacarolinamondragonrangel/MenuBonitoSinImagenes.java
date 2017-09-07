f8c4a54b3ca647d136bd641b2b992882380f4aab
ancamondragon <carobox96@gmail.com>
2017-09-07 11:30:24 -0600

    el java del reto

--- PracticaU1_1_Retosemana_AnaCarolinaMondragonRangel/app/src/main/java/mx/edu/ittepic/practicau1_1_retosemana_anacarolinamondragonrangel/MenuBonitoSinImagenes.java
+++ PracticaU1_1_Retosemana_AnaCarolinaMondragonRangel/app/src/main/java/mx/edu/ittepic/practicau1_1_retosemana_anacarolinamondragonrangel/MenuBonitoSinImagenes.java
@@ -2,6 +2,9 @@ package mx.edu.ittepic.practicau1_1_retosemana_anacarolinamondragonrangel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MenuBonitoSinImagenes extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MenuBonitoSinImagenes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_bonito_sin_imagenes);
+
+        ArrayList<View> viewsToFadeIn = new ArrayList<View>();
+        viewsToFadeIn.add(findViewById(R.id.vacio1));
+        viewsToFadeIn.add(findViewById(R.id.vacio2));
+        viewsToFadeIn.add(findViewById(R.id.internet));
+        viewsToFadeIn.add(findViewById(R.id.movil));
+        viewsToFadeIn.add(findViewById(R.id.itic));
+        viewsToFadeIn.add(findViewById(R.id.app));
+        viewsToFadeIn.add(findViewById(R.id.android));
+        for (View v : viewsToFadeIn)
+        {
+            v.setAlpha(0);
+        }
+
+        for (View v : viewsToFadeIn)
+        {
+            v.animate().alpha(1.0f).setDuration(2000).start();
+        }
     }
 }
