1ea66e04e6e1598b86d4170f1d891c9263e5bcf5
ancamondragon <carobox96@gmail.com>
2017-09-23 20:07:29 -0600

    MUERTE 5

---
+++ DADM_U2_Practica5_AnaCarolinaMondragonRangel/app/src/main/java/mx/edu/ittepic/dadm_u2_practica5_anacarolinamondragonrangel/BurgerKing.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.dadm_u2_practica5_anacarolinamondragonrangel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BurgerKing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_burger_king);
+    }
+}
