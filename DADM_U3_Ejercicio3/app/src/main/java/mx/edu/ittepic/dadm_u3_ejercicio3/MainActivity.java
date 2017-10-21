3cf4ef0042dc3d46f54deaa5458633b486dd083d
ancamondragon <carobox96@gmail.com>
2017-10-21 10:38:11 -0600

    chachan

--- DADM_U3_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u3_ejercicio3/MainActivity.java
+++ DADM_U3_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u3_ejercicio3/MainActivity.java
@@ -1,8 +1,11 @@
 package mx.edu.ittepic.dadm_u3_ejercicio3;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -14,14 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try{
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 10);
+        try {
             filtro = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
             receptor = new ReceptorSMS();
-            registerReceiver(receptor,filtro);//LO REGISTRA A NIVEL DE APLIACION ESPERANDO EL SMS ENTRANTE
+            registerReceiver(receptor, filtro);//LO REGISTRA A NIVEL DE APLIACION ESPERANDO EL SMS ENTRANTE
 
-        }catch (Exception e){
-            Toast.makeText(getApplicationContext(),"No se logro iniciar el receptor de sms entrante", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "No se logro iniciar el receptor de sms entrante", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
         }
     }
 }
