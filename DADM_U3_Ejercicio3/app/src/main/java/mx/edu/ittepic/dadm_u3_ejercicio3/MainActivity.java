21447f0da8a2e8d6dcf451c6c6cf3297f2f9d267
ancamondragon <carobox96@gmail.com>
2017-10-21 12:37:06 -0600

    eaea calalos

--- DADM_U3_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u3_ejercicio3/MainActivity.java
+++ DADM_U3_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u3_ejercicio3/MainActivity.java
@@ -8,11 +8,18 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receptor;
     IntentFilter filtro;
+    EditText numero,contenido;
+    Button enviar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,23 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "No se logro iniciar el receptor de sms entrante", Toast.LENGTH_SHORT).show();
         }
+        numero =(EditText)findViewById(R.id.numerotelefonico);
+        contenido =(EditText)findViewById(R.id.mensaje);
+        enviar =(Button) findViewById(R.id.enviar);
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num,cont;
+                num = numero.getText().toString();
+                cont = contenido.getText().toString();
+
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(num, null, cont, null,null);
+
+                Toast.makeText(MainActivity.this,"SE ENVIO MENSAJE", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
