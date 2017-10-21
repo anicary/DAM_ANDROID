9a100979d33f2d913d103b6e2fbd79cab3d522ec
ancamondragon <carobox96@gmail.com>
2017-10-21 13:18:09 -0600

    3er video

--- DADM_U3_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u3_ejercicio3/MainActivity.java
+++ DADM_U3_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u3_ejercicio3/MainActivity.java
@@ -4,8 +4,10 @@ import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -26,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 10);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 123);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+
         try {
             filtro = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
             receptor = new ReceptorSMS();
@@ -46,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 cont = contenido.getText().toString();
 
                 SmsManager sms = SmsManager.getDefault();
-                sms.sendTextMessage(num, null, cont, null,null);
+                sms.sendTextMessage(num, null, cont,null,null);
 
                 Toast.makeText(MainActivity.this,"SE ENVIO MENSAJE", Toast.LENGTH_LONG).show();
             }
@@ -56,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 10) {
+        }
+        if (requestCode == 123){
+
+        }
+        if (requestCode == 1) {
+            if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+            }
         }
     }
 }
