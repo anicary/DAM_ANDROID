4a12fe094271736f4f6f4a4aa98b80bd78232730
ancamondragon <carobox96@gmail.com>
2017-10-22 22:17:01 -0600

    p

--- DADM_MINIGAMEU3_AnaCarolina_ZulmaIsabel/app/src/main/java/mx/edu/ittepic/dadm_minigameu3_anacarolina_zulmaisabel/AcercaDe.java
+++ DADM_MINIGAMEU3_AnaCarolina_ZulmaIsabel/app/src/main/java/mx/edu/ittepic/dadm_minigameu3_anacarolina_zulmaisabel/AcercaDe.java
@@ -2,12 +2,17 @@ package mx.edu.ittepic.dadm_minigameu3_anacarolina_zulmaisabel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AcercaDe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
         setContentView(R.layout.activity_acerca_de);
     }
 }
