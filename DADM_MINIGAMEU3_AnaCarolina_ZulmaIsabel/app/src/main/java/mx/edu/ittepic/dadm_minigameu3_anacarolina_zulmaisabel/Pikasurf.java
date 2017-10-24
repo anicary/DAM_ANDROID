73ee35c1b1e34c7f2e04b8f180c7323087abc72b
fernando mac <feranimaciones@gmail.com>
2017-10-24 14:20:33 -0600

    Cosillas

--- DADM_MINIGAMEU3_AnaCarolina_ZulmaIsabel/app/src/main/java/mx/edu/ittepic/dadm_minigameu3_anacarolina_zulmaisabel/Pikasurf.java
+++ DADM_MINIGAMEU3_AnaCarolina_ZulmaIsabel/app/src/main/java/mx/edu/ittepic/dadm_minigameu3_anacarolina_zulmaisabel/Pikasurf.java
@@ -1,12 +1,19 @@
 package mx.edu.ittepic.dadm_minigameu3_anacarolina_zulmaisabel;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,6 +21,7 @@ import android.view.WindowManager;
 public class Pikasurf extends AppCompatActivity  implements SensorEventListener {
     public SensorManager sensores; //SENSOR MANAGER
     public long ultimaact; //ES UNA VARIABLE PARA DETECTAR EL SHAKE
+    int resulusionx,resulusiony;
     float x=0,y=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +62,50 @@ public class Pikasurf extends AppCompatActivity  implements SensorEventListener 
     }
 
     public class Pika extends View{
+        int xc=0,yc=0;
+        CountDownTimer timpo;
         public Pika (Context context) {
             super(context);
+            Resolucion();
+            timpo= new CountDownTimer(1,1) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    timpo.start();
+                }
+            };
+            timpo.start();
+        }
+        public void onDraw (Canvas c)
+        {
+            Paint p = new Paint();
+            p.setStyle(Paint.Style.FILL);
+            p.setColor(Color.rgb(59, 66, 167));
+            c.drawPaint(p); //PARA DIBUJAR EL PAINT
+            cuadrado(c,Color.WHITE,xc,yc,50,50);
+        }
+        public  void cuadrado (Canvas c, int color, float  x, float y, float tamx, float tamy)
+        {
+            Paint p =new Paint();
+            p.setColor(color);
+            p.setStyle(Paint.Style.STROKE); //CONTORNO
+            p.setStyle(Paint.Style.FILL); //MAS GRUESO
+            float r = x + tamx;
+            float b = y + tamy;
+            RectF rec = new RectF(x, y, r, b);
+            c.drawRect(rec, p);
+        }
+        public void Resolucion() {
+            WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            resulusionx = size.x;
+            resulusiony = size.y;
 
         }
     }
