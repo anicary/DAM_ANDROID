728fcb302899430ffe292aee9b37790fae4fca47
ancamondragon <carobox96@gmail.com>
2017-10-25 08:54:30 -0600

    UNIDAD 4

---
+++ DADM_U4_Ejercicio1/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio1/BaseDatos.java
@@ -0,0 +1,31 @@
+package mx.edu.ittepic.dadm_u4_ejercicio1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by UsuarioPrueba on 25/10/2017.
+ */
+//openHelper es como un asistente
+    //sqlite es un middleware que lo utiliza android pero no esta echo por android
+public class BaseDatos extends SQLiteOpenHelper  {
+    //activida que lo va a usar, base de datos, cursosfactory se deja en null y la version siempre va a ser 1
+    //el nombre de la BD se pone en el mainactivity
+    public BaseDatos(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //estructura de tablas que se desea crear
+        //tiene un parametro llamado db
+        db.execSQL("CREATE TABLE PERSONA(ID INT, NOMBRE VARCHAR(100),DOMICILIO VARCHAR(300))");//EJECUTA SQL
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    //MODIFICAR LA ESTRUCTURA DE UNA BD ANTERIOR EN EL MISMO PROYECTO(CAMIAR UNA O MAS TABLAS)
+        //EJEMPLO AGREGARLA A UNA PERSONA 3 COLUMNAS:TRABAJO,SUELDO Y CREDITO
+    }
+}
