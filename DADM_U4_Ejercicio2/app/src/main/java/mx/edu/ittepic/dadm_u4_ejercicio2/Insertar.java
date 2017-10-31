e786765166a56c358b2bc83bc5c8e998b439d11a
ancamondragon <carobox96@gmail.com>
2017-10-31 16:48:01 -0700

    insertar

--- DADM_U4_Ejercicio2/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio2/Insertar.java
+++ DADM_U4_Ejercicio2/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio2/Insertar.java
@@ -1,25 +1,43 @@
 package mx.edu.ittepic.dadm_u4_ejercicio2;
 
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Insertar extends AppCompatActivity {
     BD db;
+    EditText nombre, domicilio, sueldo, puesto, fecha, conyugue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insertar);
+
+        nombre = (EditText) findViewById(R.id.nombre);
+        domicilio = (EditText) findViewById(R.id.domicilio);
+        sueldo = (EditText) findViewById(R.id.sueldo);
+        puesto = (EditText) findViewById(R.id.puesto);
+        fecha = (EditText) findViewById(R.id.fecha);
+        conyugue = (EditText) findViewById(R.id.conyugue);
     }
-    public boolean onCreateOptionsMenu(Menu m){
-                                        /*nombre archivo XML*//*Nombre de la clase public*/
-        getMenuInflater().inflate(R.menu.menuinsertar,m);
+
+    public boolean onCreateOptionsMenu(Menu m) {
+
+        getMenuInflater().inflate(R.menu.menuinsertar, m);
         return true;
     }
-    public boolean onOptionsItemSelected(MenuItem elegido){
-        switch (elegido.getItemId()){
+
+    public boolean onOptionsItemSelected(MenuItem elegido) {
+        switch (elegido.getItemId()) {
             case R.id.opcinsertar:
+
+                insertarDatos();
 
                 break;
             case R.id.salir:
@@ -28,4 +46,33 @@ public class Insertar extends AppCompatActivity {
         }
         return true;
     }
+
+
+    private void insertarDatos() {
+        try {
+            SQLiteDatabase base = db.getWritableDatabase();
+            String SQL = "INSERT INTO TRABAJADOR VALUES (IDTRABAJADOR,'NOMBRE','DOMICILIO','PUESTO','SUELDO','FECHAINGRESO')";
+
+            SQL = SQL.replace("NOMBRE",nombre.getText().toString());
+            SQL = SQL.replace("DOMICILIO", domicilio.getText().toString());
+            SQL = SQL.replace("PUESTO", puesto.getText().toString());
+            SQL = SQL.replace("SUELDO", Float.parseFloat(sueldo.getText().toString()));
+
+
+            base.execSQL(SQL);//AQUI SE ESTA REALIZANDO EL INSERT
+            //LA LINEA DE ARRIBA HACE TODO MENOS CONSULTAS
+
+            Toast.makeText(this, "SE INSERTO CON EXITO", Toast.LENGTH_LONG).show();
+
+            nombre.setText("");
+            domicilio.setText("");
+
+            base.close();
+
+        } catch (SQLException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
+
+
