187ba058a810e0e2b05aaa9b0ed9183fa8d51d29
fernando mac <feranimaciones@gmail.com>
2017-11-01 12:48:13 -0700

    Act mensajes

--- DADM_U4_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio3/MainActivity.java
+++ DADM_U4_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio3/MainActivity.java
@@ -1,8 +1,16 @@
 package mx.edu.ittepic.dadm_u4_ejercicio3;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.icu.util.Calendar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,15 +18,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     EditText equipo1,anotacion1,equipo2,anotacion2,fecha;
     Button btninsertar,btneliminar;
     String idtrabajadorUltimo="";
     BD db;
+    BroadcastReceiver receptor;
+    IntentFilter filtro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(verificarPermisos())
+        {
+
+        }
+        try {
+            filtro = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+            receptor = new ReceptorSMS();
+            registerReceiver(receptor, filtro);//LO REGISTRA A NIVEL DE APLIACION ESPERANDO EL SMS ENTRANTE
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "No se logro iniciar el receptor de sms entrante", Toast.LENGTH_SHORT).show();
+        }
 
         equipo1 = (EditText) findViewById(R.id.equipo);
         anotacion1 = (EditText) findViewById(R.id.anotacion1);
@@ -42,27 +67,34 @@ public class MainActivity extends AppCompatActivity {
     private void insertarDatos(){
         try{
 
+
+
             SQLiteDatabase base =db.getWritableDatabase();
-            String query1 = "INSERT INTO RESULTADOS VALUES (1,'EQUIPO1','ANOTACIONEQUIPO1','EQUIPO2','ANOTACIONEQUIPO2','FECHA')";
+            /* METODO LARGO SIMPLE*/
+           /* String cam1,cam2,cam3,cam4,cam5;
+            cam1=equipo1.getText().toString();
+            cam2=anotacion1.getText().toString();
+            cam3=equipo2.getText().toString();
+            cam4=anotacion2.getText().toString();
+            cam5=fecha.getText().toString();
+            String query1 = "INSERT INTO RESULTADOS VALUES (1,'"+cam1+"',"+cam2+",'"+cam3+"',"+cam4+",'"+cam5+"')";
+            */
+            /* METODO LARGO SIMPLE FIN*/
+            /* METODO CONFUSO SIMPLE */
+                   /* String query1 = "INSERT INTO RESULTADOS VALUES (1,'"+equipo1.getText().toString()+"',"+anotacion1.getText().toString()+"," +
+                    "'"+equipo2.getText().toString()+"',"+anotacion2.getText().toString()+",'"+fecha.getText().toString()+"')"; */
+               /* METODO CONFUSO SIMPLE FIN */
 
-            String query2 = "SELECT MAX(IDRESULTADO) FROM RESULTADOS";
-
+            /* METODO LARGO */
+            String query1 = "INSERT INTO RESULTADOS VALUES (null,'EQUIPO1',AN1,'EQUIPO2',AN2,'FECHA')";
             query1 = query1.replace("EQUIPO1",equipo1.getText().toString());
-            query1 = query1.replace("ANOTACIONEQUIPO1",anotacion1.getText().toString());
+            query1 = query1.replace("AN1",anotacion1.getText().toString());
             query1 = query1.replace("EQUIPO2",equipo2.getText().toString());
-            query1 = query1.replace("ANOTACIONEQUIPO2",anotacion2.getText().toString());
+            query1 = query1.replace("AN2",anotacion2.getText().toString());
             query1 = query1.replace("FECHA",fecha.getText().toString());
-
+            /* METODO LARGO  FIN*/
             base.execSQL(query1);
-            Cursor resultado = base.rawQuery(query2,null); //retorna un cursor
-            if (resultado.moveToFirst()) {
-                do {
-                    idtrabajadorUltimo=resultado.getString(0);
-                } while (resultado.moveToNext());
-            }
-
             Toast.makeText(this,"SE INSERTO CON EXITO",Toast.LENGTH_LONG).show();
-
             equipo1.setText("");
             anotacion1.setText("");
             equipo2.setText("");
@@ -74,5 +106,34 @@ public class MainActivity extends AppCompatActivity {
         }catch (SQLException e){
             Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
         }
+    }
+    /* PERMISOS */
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private  boolean verificarPermisos() {
+        int permisoEnviarMensajes = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+        int estadoTelefono = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        int permisoRecivirMensajes = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        int permisoLeerMensajes = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
+        List<String> listaPermisos = new ArrayList<>();
+        if (estadoTelefono != PackageManager.PERMISSION_GRANTED) {
+            listaPermisos.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (permisoEnviarMensajes != PackageManager.PERMISSION_GRANTED) {
+            listaPermisos.add(Manifest.permission.SEND_SMS);
+        }
+        if (permisoRecivirMensajes != PackageManager.PERMISSION_GRANTED) {
+            listaPermisos.add(Manifest.permission.RECEIVE_SMS);
+        }
+        if (permisoLeerMensajes != PackageManager.PERMISSION_GRANTED) {
+            listaPermisos.add(Manifest.permission.READ_SMS);
+        }
+        if (!listaPermisos.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listaPermisos.toArray(new String[listaPermisos.size()]),1);
+            return false;
+        }
+        return true;
     }
 }
---
+++ DADM_U4_Ejercicio3/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio3/ReceptorSMS.java
@@ -0,0 +1,98 @@
+package mx.edu.ittepic.dadm_u4_ejercicio3;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ReceptorSMS  extends BroadcastReceiver {
+    BD db;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        db = new BD(context,"prueba1",null,1);
+        Bundle b = intent.getExtras();
+        try {
+            if (b != null) {
+                final Object[] entrantes =(Object[]) b.get("pdus");
+                for (int i=0; i<entrantes.length; i++){
+                    SmsMessage mensaje = SmsMessage.createFromPdu((byte[])entrantes[i]);
+                    String numero, contenido;
+                    numero = mensaje.getDisplayOriginatingAddress();
+                    contenido = mensaje.getDisplayMessageBody();
+                    SmsManager sms = SmsManager.getDefault();
+                    String comparador=contenido.substring(0,10);
+                    if(comparador.startsWith("!RESULTADO")){
+                        String fecha=contenido.substring(11,contenido.length());
+                        SQLiteDatabase base =db.getWritableDatabase();
+                        String query1 = "SELECT * FROM RESULTADOS WHERE FECHA='"+fecha+"';";
+                        System.out.println("fecha"+fecha);
+                        Cursor resultado = base.rawQuery(query1,null); //retorna un cursor
+                        if (resultado.moveToFirst()) {
+                            sms.sendTextMessage(numero, null, "LOS RESULTADOS PARA LA FECHA "+fecha+" SON:", null, null);
+                            String mensajes[] = new String[resultado.getCount()];
+                            int pos=0;
+                            do {
+                                Toast.makeText(context,"MENSAJE "+fecha+"", Toast.LENGTH_SHORT).show();
+                                mensajes[pos]=""+resultado.getString(1)+""+":"+resultado.getString(2)+"--"+resultado.getString(3)+""+":"+resultado.getString(4);
+                                pos++;
+                            } while (resultado.moveToNext());
+
+                            for(int e=0;e<mensajes.length;e++)
+                            {
+                                sms.sendTextMessage(numero, null,""+ xonvertidorNonAscii(mensajes[e]), null, null);
+                            }
+
+                        }else
+                        {
+                            sms.sendTextMessage(numero, null,"NO HAY PARTIDOS EN ESA FECHA  ", null, null);
+                        }
+
+                    }else{
+                        Toast.makeText(context,"error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }catch (Exception e){
+            Toast.makeText(context,"NO SE PUDO ENVIAR EL MENSAJE", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private static final String PLAIN_ASCII = "AaEeIiOoUu"
+            + "AaEeIiOoUuYy"
+            + "AaEeIiOoUuYy"
+            + "AaOoNn"
+            + "AaEeIiOoUuYy"
+            + "Aa"
+            + "Cc"
+            + "OoUu"
+            ;
+    private static final String UNICODE = "\u00C0\u00E0\u00C8\u00E8\u00CC\u00EC\u00D2\u00F2\u00D9\u00F9"
+            + "\u00C1\u00E1\u00C9\u00E9\u00CD\u00ED\u00D3\u00F3\u00DA\u00FA\u00DD\u00FD"
+            + "\u00C2\u00E2\u00CA\u00EA\u00CE\u00EE\u00D4\u00F4\u00DB\u00FB\u0176\u0177"
+            + "\u00C3\u00E3\u00D5\u00F5\u00D1\u00F1"
+            + "\u00C4\u00E4\u00CB\u00EB\u00CF\u00EF\u00D6\u00F6\u00DC\u00FC\u0178\u00FF"
+            + "\u00C5\u00E5" + "\u00C7\u00E7" + "\u0150\u0151\u0170\u0171";
+
+    public static String xonvertidorNonAscii(String s) {
+        if (s == null)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            int pos = UNICODE.indexOf(c);
+            if (pos > -1) {
+                sb.append(PLAIN_ASCII.charAt(pos));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
