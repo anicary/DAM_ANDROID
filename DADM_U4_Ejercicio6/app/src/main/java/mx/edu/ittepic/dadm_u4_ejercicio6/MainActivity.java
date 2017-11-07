20f70b02108e876d31989b52f72554856d4f45e1
fernando mac <feranimaciones@gmail.com>
2017-11-06 22:50:44 -0700

    Act con animacion

--- DADM_U4_Ejercicio6/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio6/MainActivity.java
+++ DADM_U4_Ejercicio6/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio6/MainActivity.java
@@ -1,7 +1,10 @@
 package mx.edu.ittepic.dadm_u4_ejercicio6;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     Random pc,jugador;
     int dadoj1num=0,dadoj2num=0;
     int sumj=0,sumcpu=0;
+    Thread thread;
+    boolean jugando=false,p=false,cpu=false;
+    int pos=0,pos2=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +39,98 @@ public class MainActivity extends AppCompatActivity {
         tirar =(Button)findViewById(R.id.tirar);
         turno=(TextView)findViewById(R.id.turno);
         turno.setText("TURNO JUGADOR");
+
+        new Thread() {
+            public void run() {
+                while (true) {
+                    try {
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                if(jugando)
+                                {
+                                    if(p)
+                                    {
+                                        tirar.setEnabled(false);
+                                        if(pos==0)
+                                        {
+                                            dadoj1num=jugador.nextInt(6)+1;
+                                            dadoJ1.setText(""+dadoj1num);
+                                        }
+                                        if(pos==1)
+                                        {
+                                            dadoj2num=jugador.nextInt(6)+1;
+                                            sumj+=dadoj1num+dadoj2num;
+                                            dadoJ2.setText(""+dadoj2num);
+
+                                        }
+                                        if(pos==2)
+                                        {
+                                            total2.setText(""+sumj);
+                                            turno.setText("TURNO CPU");
+                                            p=false;
+                                           // jugando=false;
+                                            pos=0;
+                                            pos2=0;
+                                            cpu=true;
+                                            return;
+                                        }
+                                        pos++;
+                                        //Toast.makeText(MainActivity.this,"EL JUGADOR GANA",Toast.LENGTH_SHORT).show();
+                                    }
+                                    if(cpu)
+                                    {
+
+                                        if(pos2==0)
+                                        {
+                                            //Toast.makeText(MainActivity.this,"CPU",Toast.LENGTH_SHORT).show();
+                                            dadoj1num=jugador.nextInt(6)+1;
+                                            dado1.setText(""+dadoj1num);
+                                        }
+                                        if(pos2==1)
+                                        {
+                                           // Toast.makeText(MainActivity.this,"CPU",Toast.LENGTH_SHORT).show();
+                                            dadoj2num=jugador.nextInt(6)+1;
+                                            sumcpu+=dadoj1num+dadoj2num;
+                                            dado2.setText(""+dadoj2num);
+
+                                        }
+                                        if(pos2==2)
+                                        {
+                                            total1.setText(""+sumcpu);
+                                            turno.setText("TURNO JUGADOR");
+                                           // p=true;
+                                          //  jugando=false;
+                                            pos=0;
+                                            pos2=0;
+                                            cpu=false;
+                                            tirar.setEnabled(true);
+                                            return;
+                                        }
+                                        pos2++;
+                                        //Toast.makeText(MainActivity.this,"EL JUGADOR GANA",Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            }
+                        });
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+
         tirar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                jugando=true;
+                p=true;
                 if(turnojugada<3)
                 {
+                    /*
                     tirar.setEnabled(false);
                     dadoj1num=jugador.nextInt(6)+1;
                     dadoj2num=jugador.nextInt(6)+1;
@@ -47,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
                     total2.setText(""+sumj);
                     turno.setText("TURNO CPU");
                     cpu();
-                    turnojugada++;
+                    turnojugada++;*/
                 }
             }
         });
@@ -55,14 +148,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void cpu()
     {
-        dadoj1num=jugador.nextInt(6)+1;
-        dadoj2num=jugador.nextInt(6)+1;
-        sumcpu+=dadoj1num+dadoj2num;
-        dado1.setText(""+dadoj1num);
-        dado2.setText(""+dadoj2num);
-        total1.setText(""+sumcpu);
-        tirar.setEnabled(true);
-        turno.setText("TURNO JUGADOR");
+      /*
         if(turnojugada>=2)
         {
             if(sumj>sumcpu)
@@ -84,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
 
             turnojugada=0;
         }
-        System.out.println(""+turnojugada);
+        System.out.println(""+turnojugada);*/
     }
 
 }
