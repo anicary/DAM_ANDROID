5741e7b29318849cc60205e6a4b144d5cbbf780e
fernando mac <feranimaciones@gmail.com>
2017-11-10 12:21:42 -0700

    Act

--- DADM_U4_Ejercicio8/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio8/MainActivity.java
+++ DADM_U4_Ejercicio8/app/src/main/java/mx/edu/ittepic/dadm_u4_ejercicio8/MainActivity.java
@@ -10,19 +10,18 @@ import android.widget.Toast;
 
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
-
 public class MainActivity extends AppCompatActivity {
     int dados[] = {R.drawable.d1, R.drawable.d2, R.drawable.d3, R.drawable.d4, R.drawable.d5, R.drawable.d6};
     Thread[] jugadores;
     Random pc;
     Button inicar;
-    //boolean jugar = false;
     boolean[] jugadas;
     int[][] puntos;
     int[] totales;
     ImageView[][] dadosImagenes;
     TextView[] txtTotales;
     int turno = 0;
+    int tim=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!jugadas[0]) {
-                    //  jugar = true;
                     jugadas[0] = true;
                     jugadas[1] = false;
                     jugadas[2] = false;
@@ -70,16 +68,21 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void run() {
                                         if (turno < 3) {
-                                            //  if (jugar) {
+                                            if(tim==1)
+                                            {
+                                                jugadas[0] = false;
+                                                jugadas[1] = true;
+                                                tim=0;
+                                            }else
+                                            {
                                                 puntos[0][0] = pc.nextInt(6) + 1;
                                                 puntos[0][1] = pc.nextInt(6) + 1;
                                                 dadosImagenes[0][0].setImageDrawable(getResources().getDrawable(dados[puntos[0][0] - 1]));
                                                 dadosImagenes[0][1].setImageDrawable(getResources().getDrawable(dados[puntos[0][1] - 1]));
                                                 totales[0] += puntos[0][0] + puntos[0][1];
                                                 txtTotales[0].setText("" + totales[0]);
-                                                jugadas[0] = false;
-                                                jugadas[1] = true;
-                                            //  }
+                                                tim++;
+                                            }
                                         } else {
                                             if (totales[0] > totales[1] && totales[0] > totales[2] && totales[0] > totales[3]) {
                                                 Toast.makeText(MainActivity.this, "El ganador es jugador 1", Toast.LENGTH_SHORT).show();
@@ -93,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
                                                         Toast.makeText(MainActivity.this, "El ganador es jugador 4", Toast.LENGTH_SHORT).show();
                                                 }
                                             }
-                                            //jugar = false;
                                             jugadas[0] = false;
                                             turno = 0;
                                             for (int i = 0; i < totales.length; i++) {
@@ -120,20 +122,24 @@ public class MainActivity extends AppCompatActivity {
                                 @Override
                                 public void run() {
                                     if (turno < 3) {
-                                        // if (jugar) {
-                                        puntos[1][0] = pc.nextInt(6) + 1;
-                                        dadosImagenes[1][0].setImageDrawable(getResources().getDrawable(dados[puntos[1][0] - 1]));
-                                        puntos[1][1] = pc.nextInt(6) + 1;
-                                        dadosImagenes[1][1].setImageDrawable(getResources().getDrawable(dados[puntos[1][1] - 1]));
-                                        totales[1] += puntos[1][0] + puntos[1][1];
-                                        txtTotales[1].setText("" + totales[1]);
-                                        jugadas[1] = false;
-                                        jugadas[2] = true;
-                                        //   }
+                                        if(tim==1)
+                                        {
+                                            jugadas[1] = false;
+                                            jugadas[2] = true;
+                                            tim=0;
+                                        }else
+                                        {
+                                            puntos[1][0] = pc.nextInt(6) + 1;
+                                            dadosImagenes[1][0].setImageDrawable(getResources().getDrawable(dados[puntos[1][0] - 1]));
+                                            puntos[1][1] = pc.nextInt(6) + 1;
+                                            dadosImagenes[1][1].setImageDrawable(getResources().getDrawable(dados[puntos[1][1] - 1]));
+                                            totales[1] += puntos[1][0] + puntos[1][1];
+                                            txtTotales[1].setText("" + totales[1]);
+                                            tim++;
+                                        }
                                     }
                                 }
                             });
-
                         }
                         sleep(1000);
                     }
@@ -150,16 +156,22 @@ public class MainActivity extends AppCompatActivity {
                                 @Override
                                 public void run() {
                                     if (turno < 3) {
-                                        //   if (jugar) {
-                                        puntos[2][0] = pc.nextInt(6) + 1;
-                                        dadosImagenes[2][0].setImageDrawable(getResources().getDrawable(dados[puntos[2][0] - 1]));
-                                        puntos[2][1] = pc.nextInt(6) + 1;
-                                        dadosImagenes[2][1].setImageDrawable(getResources().getDrawable(dados[puntos[2][1] - 1]));
-                                        totales[2] += puntos[2][0] + puntos[2][1];
-                                        txtTotales[2].setText("" + totales[2]);
-                                        jugadas[2] = false;
-                                        jugadas[3] = true;
-                                        //      }
+                                        if(tim==1)
+                                        {
+
+                                            jugadas[2] = false;
+                                            jugadas[3] = true;
+                                            tim=0;
+                                        }else
+                                        {
+                                            puntos[2][0] = pc.nextInt(6) + 1;
+                                            dadosImagenes[2][0].setImageDrawable(getResources().getDrawable(dados[puntos[2][0] - 1]));
+                                            puntos[2][1] = pc.nextInt(6) + 1;
+                                            dadosImagenes[2][1].setImageDrawable(getResources().getDrawable(dados[puntos[2][1] - 1]));
+                                            totales[2] += puntos[2][0] + puntos[2][1];
+                                            txtTotales[2].setText("" + totales[2]);
+                                            tim++;
+                                        }
                                     }
                                 }
                             });
@@ -173,28 +185,35 @@ public class MainActivity extends AppCompatActivity {
         jugadores[3] = new Thread() {
             public void run() {
                 try {
+
                     while (true) {
                         if (jugadas[3]) {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
                                     if (turno < 3) {
-                                        //    if (jugar) {
-                                        puntos[3][0] = pc.nextInt(6) + 1;
-                                        dadosImagenes[3][0].setImageDrawable(getResources().getDrawable(dados[puntos[3][0] - 1]));
-                                        puntos[3][1] = pc.nextInt(6) + 1;
-                                        dadosImagenes[3][1].setImageDrawable(getResources().getDrawable(dados[puntos[3][1] - 1]));
-                                        totales[3] += puntos[3][0] + puntos[3][1];
-                                        txtTotales[3].setText("" + totales[3]);
-                                        jugadas[3] = false;
-                                        jugadas[0] = true;
-                                        turno++;
-                                        //  }
+                                        if(tim==1)
+                                        {
+                                            jugadas[3] = false;
+                                            jugadas[0] = true;
+                                            tim=0;
+                                            turno++;
+                                        }else {
+                                            tim++;
+                                            puntos[3][0] = pc.nextInt(6) + 1;
+                                            dadosImagenes[3][0].setImageDrawable(getResources().getDrawable(dados[puntos[3][0] - 1]));
+                                            puntos[3][1] = pc.nextInt(6) + 1;
+                                            dadosImagenes[3][1].setImageDrawable(getResources().getDrawable(dados[puntos[3][1] - 1]));
+                                            totales[3] += puntos[3][0] + puntos[3][1];
+                                            txtTotales[3].setText("" + totales[3]);
+
+                                        }
                                     }
                                 }
                             });
                         }
-                        sleep(3000);
+
+                        sleep(1000);
                     }
                 } catch (InterruptedException e) {
                 }
