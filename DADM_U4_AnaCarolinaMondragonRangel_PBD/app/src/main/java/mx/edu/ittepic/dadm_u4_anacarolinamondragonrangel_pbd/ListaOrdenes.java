3d48f4fdd58002ff1659b3e0d98d8e09ec3b5ccd
fernando <feranimaciones@gmail.com>
2017-11-12 15:05:08 -0700

    act

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaOrdenes.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaOrdenes.java
@@ -9,5 +9,6 @@ public class ListaOrdenes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_ordenes);
+        setTitle("ORDENES DE REPARACION");
     }
 }
