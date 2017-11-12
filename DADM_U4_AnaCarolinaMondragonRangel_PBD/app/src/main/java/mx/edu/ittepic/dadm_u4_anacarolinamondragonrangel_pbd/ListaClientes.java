6dc5633b8f0d4661f5dd8bef4fe6cb184b16fa0e
fernando <feranimaciones@gmail.com>
2017-11-11 18:33:46 -0700

    Act

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaClientes.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaClientes.java
@@ -1,6 +1,8 @@
 package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_pbd;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +16,15 @@ public class ListaClientes extends AppCompatActivity {
     ArrayList<Cliente> elemento;
     ListView Menu_lista;
     Button agregar;
+    BD conexion;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_clientes);
         Menu_lista=(ListView)findViewById(R.id.listaClientes);
+        conexion= new BD(ListaClientes.this, "prueba1", null, 1);
         elemento = getElemento();
         agregar=(Button) findViewById(R.id.btnAgregar);
         agregar.setOnClickListener(new View.OnClickListener() {
@@ -32,12 +38,15 @@ public class ListaClientes extends AppCompatActivity {
         Menu_lista.setAdapter(adater);
     }
     private ArrayList<Cliente> getElemento() {
+        SQLiteDatabase base = conexion.getReadableDatabase();
+        Cursor c = base.rawQuery("SELECT * FROM cliente", null);
         ArrayList<Cliente> elementos = new ArrayList<Cliente>();
-        elementos.add(new Cliente(1,"TEST","TEST ","test"));
-       /* for(int i=0;i<ProductoNombre.length;i++)
+        if (c.moveToFirst() == true) {
+            elementos.add(new Cliente(Integer.parseInt(c.getString(0)),c.getString(1),c.getString(2),c.getString(3)));
+        }else
         {
-            elementos.add(new Producto(i, ProductoNombre[i], ProductoPeso[i],ProductoCosto[i], idProducto[i],"@drawable/imagendulce"));
-        }*/
+            elementos.add(new Cliente(1,"NO EXISTEN CLIENTES","  "," "));
+        }
         return elementos;
     }
 }
