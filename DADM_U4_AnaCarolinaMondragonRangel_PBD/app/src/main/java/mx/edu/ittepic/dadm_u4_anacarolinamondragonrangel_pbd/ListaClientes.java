89f8dbe7e3fea7dc72a2b7ff6f588d1883483cc4
fernando <feranimaciones@gmail.com>
2017-11-11 18:22:50 -0700

    Act

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaClientes.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaClientes.java
@@ -1,7 +1,10 @@
 package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_pbd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -10,14 +13,22 @@ public class ListaClientes extends AppCompatActivity {
     ADcliente adater;
     ArrayList<Cliente> elemento;
     ListView Menu_lista;
+    Button agregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_clientes);
         Menu_lista=(ListView)findViewById(R.id.listaClientes);
         elemento = getElemento();
+        agregar=(Button) findViewById(R.id.btnAgregar);
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento= new Intent(ListaClientes.this,AgregarCliente.class);
+                startActivity(intento);
+            }
+        });
         adater = new ADcliente(this, elemento);
-
         Menu_lista.setAdapter(adater);
     }
     private ArrayList<Cliente> getElemento() {
