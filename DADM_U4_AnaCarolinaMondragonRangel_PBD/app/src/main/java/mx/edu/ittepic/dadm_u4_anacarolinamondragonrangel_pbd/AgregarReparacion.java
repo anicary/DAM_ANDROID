e84381f11f375c7372b45c030b501aa28c4eb91b
ancamondragon <carobox96@gmail.com>
2017-11-12 16:18:35 -0700

    falta

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/AgregarReparacion.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/AgregarReparacion.java
@@ -2,12 +2,25 @@ package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_pbd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AgregarReparacion extends AppCompatActivity {
-
+    EditText ed1,ed2;
+    Spinner SpinnerCliente;
+    BD db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_reparacion);
+
+        ed1=(EditText)findViewById(R.id.ed1);
+        ed2=(EditText)findViewById(R.id.ed2);
+
+        SpinnerCliente=(Spinner)findViewById(R.id.spinnerCliente);
+        
+
     }
 }
