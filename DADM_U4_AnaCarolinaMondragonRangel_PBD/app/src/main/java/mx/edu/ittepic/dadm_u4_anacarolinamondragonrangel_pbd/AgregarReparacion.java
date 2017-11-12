b7193fa2e00aa103b462072e1a965902fa8957be
ancamondragon <carobox96@gmail.com>
2017-11-12 16:56:21 -0700

    ya

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/AgregarReparacion.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/AgregarReparacion.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 public class AgregarReparacion extends AppCompatActivity {
     EditText ed1,ed2;
     Spinner SpinnerCliente;
+    Button btnagregarCliente;
     BD db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,8 @@ public class AgregarReparacion extends AppCompatActivity {
 
         ed1=(EditText)findViewById(R.id.fecha);
         ed2=(EditText)findViewById(R.id.costo);
+
+        btnagregarCliente=(Button) findViewById(R.id.btnagregar);
 
         SpinnerCliente=(Spinner)findViewById(R.id.spinnerCliente);
     }
