c43fb3d13dbfccabd2d007c3140443f52a073714
ancamondragon <carobox96@gmail.com>
2017-11-13 11:55:25 -0700

    ya?

--- DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/AgregarContacto.java
+++ DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/AgregarContacto.java
@@ -2,12 +2,24 @@ package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_bdremota;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AgregarContacto extends AppCompatActivity {
+    EditText nombre,domicilio,telefono,correo;
+    Button agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_contacto);
+
+        agregar =(Button)findViewById(R.id.btnagregar);
+
+        nombre =(EditText)findViewById(R.id.nombre);
+        domicilio =(EditText)findViewById(R.id.dom);
+        telefono =(EditText)findViewById(R.id.tel);
+        correo =(EditText)findViewById(R.id.correo);
+
     }
 }
---
+++ DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/ConexionRemota.java
@@ -0,0 +1,8 @@
+package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_bdremota;
+
+/**
+ * Created by UsuarioPrueba on 13/11/2017.
+ */
+
+public class ConexionRemota {
+}
