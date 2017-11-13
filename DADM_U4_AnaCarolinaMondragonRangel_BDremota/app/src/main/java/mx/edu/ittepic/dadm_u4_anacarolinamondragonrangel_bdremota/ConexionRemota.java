80b8bc443a9f4d30a0edcbd086604c43e809406c
ancamondragon <carobox96@gmail.com>
2017-11-13 12:15:30 -0700

    ..

--- DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/ConexionRemota.java
+++ DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/ConexionRemota.java
@@ -1,8 +1,35 @@
 package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_bdremota;
 
+import android.os.AsyncTask;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by UsuarioPrueba on 13/11/2017.
  */
 
-public class ConexionRemota {
+public class ConexionRemota extends AsyncTask<URL,Void,String> {
+    List<String[]> variables;
+    MainActivity puntero;
+
+    public void ConexionWeb(MainActivity p)
+    {
+        variables= new ArrayList<>();
+        puntero=p;
+    }
+    public void agregarValores(String indentificador,String Datos)
+    {
+        String[] temp={indentificador,Datos};
+        variables.add(temp);
+    }
+    public void limpiarVariables(){
+        variables= new ArrayList<String[]>();
+    }
+
+    @Override
+    protected String doInBackground(URL... urls) {
+        return null;
+    }
 }
