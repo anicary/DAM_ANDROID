71faded53e98c2872e86aa48c0a2dc1382ddb2b8
ancamondragon <carobox96@gmail.com>
2017-11-13 11:40:47 -0700

    BASE

--- DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/MainActivity.java
+++ DADM_U4_AnaCarolinaMondragonRangel_BDremota/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_bdremota/MainActivity.java
@@ -1,13 +1,32 @@
 package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_bdremota;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lista;
+    Button agregarcontacto;
+    Intent intento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        agregarcontacto =(Button)findViewById(R.id.agregarcon);
+
+        lista =(ListView)findViewById(R.id.lista);
+
+        agregarcontacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intento =new Intent(MainActivity.this,AgregarContacto.class);
+                startActivity(intento);
+            }
+        });
     }
 }
