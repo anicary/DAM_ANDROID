b5117fe094f7e4389114c16cbb5c3117d8a42690
ancamondragon <carobox96@gmail.com>
2017-11-12 18:26:03 -0700

    .

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/AgregarEquipo.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/AgregarEquipo.java
@@ -1,14 +1,22 @@
 package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_pbd;
 
+import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AgregarEquipo extends AppCompatActivity {
     EditText descripcion,tipo;
+    String description, type;
+    int idordenreparacion;
     Button btnagregarEquipo;
+    Intent intent;
     BD db;
 
     @Override
@@ -20,5 +28,28 @@ public class AgregarEquipo extends AppCompatActivity {
         tipo=(EditText)findViewById(R.id.tipo);
         btnagregarEquipo=(Button) findViewById(R.id.btnagregarEquipo);
 
+        db = new BD(AgregarEquipo.this, "reparacionCelular", null, 1);
+        btnagregarEquipo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                idordenreparacion = (getIntent().getExtras().getInt("idordenreparacion"));
+                type = tipo.getText().toString();
+                try {
+                    SQLiteDatabase base = db.getWritableDatabase();
+                    String query1 = "INSERT INTO aparato VALUES (null,'DESCRIPCION','TIPO')";
+                    query1 = query1.replace("DESCRIPCION", description);
+                    query1 = query1.replace("TIPO",type);
+
+                    base.execSQL(query1);
+                    intent = new Intent(AgregarEquipo.this, ListaClientes.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }catch (SQLException e){
+                    Toast.makeText(AgregarEquipo.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
