287670cbe8bc587566320b544bbd2f781317e03b
fernando <feranimaciones@gmail.com>
2017-11-12 17:49:14 -0700

    axt

--- DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaOrdenes.java
+++ DADM_U4_AnaCarolinaMondragonRangel_PBD/app/src/main/java/mx/edu/ittepic/dadm_u4_anacarolinamondragonrangel_pbd/ListaOrdenes.java
@@ -1,13 +1,17 @@
 package mx.edu.ittepic.dadm_u4_anacarolinamondragonrangel_pbd;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,11 +22,14 @@ Button botonAgregar;
     BD conexion;
     ArrayList<Cliente> elementos;
     ListView Menu_lista;
+    AlertDialog.Builder alertaBorrar;
+    int idClienteborrar,poslista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_ordenes);
         setTitle("ORDENES DE REPARACION");
+        conexion = new BD(ListaOrdenes.this, "reparacionCelular", null, 1);
         botonAgregar=(Button)findViewById(R.id.btnCrear);
         botonAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,8 +39,48 @@ Button botonAgregar;
             }
         });
         Menu_lista = (ListView) findViewById(R.id.idlistareparacion);
-        conexion = new BD(ListaOrdenes.this, "reparacionCelular", null, 1);
         elemento = getElemento();
+        adater = new ADcliente(this, elemento, new ADcliente.botonClick() {
+            @Override
+            public void onBtnClick(int position) {
+                Intent intento = new Intent(ListaOrdenes.this,EditarCliente.class);
+                intento.putExtra("idcliente",elementos.get(position).getIdcliente() );
+                intento.putExtra("nombre",elementos.get(position).getNombre() );
+                intento.putExtra("domicilio",elementos.get(position).getDomicilio() );
+                intento.putExtra("colonia",elementos.get(position).getColonia() );
+                startActivity(intento);
+            }
+        }, new ADcliente.botonClick() {
+            @Override
+            public void onBtnClick(int position) {
+                idClienteborrar= elementos.get(position).getIdcliente();
+                poslista=position;
+                alertaBorrar = new AlertDialog.Builder(ListaOrdenes.this);
+                alertaBorrar.setIcon(R.drawable.ic_delete_forever_black_24dp)
+                        .setTitle("Eliminar a este cliente")
+                        .setMessage("Nombre: "+  elementos.get(position).getNombre())
+                        .setPositiveButton("ELIMINAR", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                try {
+                                    System.out.println(""+idClienteborrar);
+                                    elementos.remove(poslista);
+                                    Menu_lista.setAdapter(adater);
+                                    SQLiteDatabase base = conexion.getWritableDatabase();
+                                    String query1 = "DELETE FROM cliente  where idcliente=" + idClienteborrar + ";";
+                                    base.execSQL(query1);
+                                } catch (SQLException e) {
+                                    Toast.makeText(ListaOrdenes.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertaBorrar.show();
+            }
+        });
         Menu_lista.setAdapter(adater);
     }
     private ArrayList<Cliente> getElemento() {
