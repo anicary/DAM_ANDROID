1f470951d5ad34d3f9876321cacf7896e8a7398d
ancamondragon <carobox96@gmail.com>
2017-11-13 14:05:02 -0700

    new

---
+++ DADM_U4_AnaCarolinaMondragonRangel_BDremota/basedatos/seleccionarcontacto.php
@@ -0,0 +1,32 @@
+<?php
+/* DATOS DEL SERVIDOR */
+$contrasna="Carolina21";
+$usuario="u146418920_caro";
+$host="mysql.hostinger.mx";
+$base="u146418920_andro";
+
+/* CONECTANDO AL SERVIDOR */
+$conexion=mysqli_connect($host,$usuario,$contrasna);
+if(!$conexion){
+	echo "La conexion fallo";
+	return;
+}
+$selectBD=mysqli_select_db($conexion,$base);
+if(!$selectBD){
+	echo "Base de datos no encontrada";
+	return;
+}
+/*OBTENER DATOS DEL ANDROD POST*/
+$nombre=$_POST["nombre"];
+$domicilio=$_POST["domicilio"];
+$telefono=$_POST["telefono"];
+$correo=$_POST["correo"];
+/* INSERTAR EN LA BASE*/
+$resultadoInsertar=mysqli_query($conexion,"INSERT INTO Persona values(0,'$nombre','$domicilio','$telefono','$correo')");
+if($resultadoInsertar){
+	 /*SI SE INSERTO*/
+}else{
+	 /*NO SE INSERTO*/
+	echo "ERROR";
+}
+?>
