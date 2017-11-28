90a90d4c0b7a5bd165c7065a2bc537b0b285efcd
ancamondragon <carobox96@gmail.com>
2017-11-28 14:01:34 -0700

    nuevo

---
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.dadm_proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
