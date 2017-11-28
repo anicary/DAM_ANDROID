157e9008ccd5e9b7d2ed7b73060ed1ede8ffcda5
ancamondragon <carobox96@gmail.com>
2017-11-28 15:05:55 -0700

    mascota

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/agregar_mascota.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/agregar_mascota.java
@@ -2,12 +2,24 @@ package mx.edu.ittepic.dadm_proyectofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class agregar_mascota extends AppCompatActivity {
+    EditText nombre,edad;
+    Spinner tipo,raza,sexo;
+    Button agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_mascota);
+        nombre =(EditText)findViewById(R.id.nombremascota);
+        edad = (EditText)findViewById(R.id.edadmascota);
+        sexo=(Spinner)findViewById(R.id.sexomascota);
+        tipo=(Spinner)findViewById(R.id.tipomascota);
+        raza=(Spinner)findViewById(R.id.razamascota);
+        agregar=(Button)findViewById(R.id.agregar);
     }
 }
