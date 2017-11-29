289f512cd16ecd93ca94400168a4b436d74980b1
ancamondragon <carobox96@gmail.com>
2017-11-29 16:20:20 -0700

    :

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/agregar_mascota.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/agregar_mascota.java
@@ -1,25 +1,64 @@
 package mx.edu.ittepic.dadm_proyectofinal;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class agregar_mascota extends AppCompatActivity {
-    EditText nombre,edad;
-    Spinner tipo,raza,sexo;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class agregar_mascota extends AppCompatActivity implements AsyncResponse {
+    EditText nombre, edad;
+    String name, sex, edad1;
+    Spinner tipo, raza, sexo;
     Button agregar;
+    ConexionWeb conexionWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_mascota);
-        nombre =(EditText)findViewById(R.id.nombremascota);
-        edad = (EditText)findViewById(R.id.edadmascota);
-        sexo=(Spinner)findViewById(R.id.sexomascota);
-        tipo=(Spinner)findViewById(R.id.tipomascota);
-        raza=(Spinner)findViewById(R.id.razamascota);
-        agregar=(Button)findViewById(R.id.agregar);
+        nombre = (EditText) findViewById(R.id.nombremascota);
+        edad = (EditText) findViewById(R.id.edadmascota);
+        sexo = (Spinner) findViewById(R.id.sexomascota);
+        tipo = (Spinner) findViewById(R.id.tipomascota);
+        raza = (Spinner) findViewById(R.id.razamascota);
+        agregar = (Button) findViewById(R.id.agregar);
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = nombre.getText().toString();
+                sex = sexo.getSelectedItem().toString();
+                edad1 = edad.getText().toString();
+
+                if (!name.equals("") || !sex.equals("") || !edad1.equals("")) {
+                    //TODO PENDIENTE
+                    try {
+                        conexionWeb = new ConexionWeb(agregar_mascota.this);
+                        conexionWeb.agregarVariables("nombre", name);
+                        conexionWeb.agregarVariables("sexo", sex);
+                        conexionWeb.agregarVariables("edad", edad1);
+                        conexionWeb.agregarVariables("tipo_mascota_idtipo_mascota", "1");
+                        conexionWeb.agregarVariables("razamascota_idrazamascota", "1");
+                        URL direccion = new URL("http://carolina.x10host.com/index.php/Sistema/registro_mascota");
+                        Intent inicio = new Intent(agregar_mascota.this,MainActivity.class);
+                        startActivity(inicio);
+                        conexionWeb.execute(direccion);
+                    } catch (MalformedURLException e) {
+                        Toast.makeText(agregar_mascota.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            }
+        });
     }
+    @Override
+    public void procesarRespuesta(String r){}
 }
