86be6a3e24920b88bc269b9992be4e9f4cfea046
ancamondragon <carobox96@gmail.com>
2017-11-28 17:52:18 -0700

    ..

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Perfil extends AppCompatActivity {
-
+    TextView nombre,apellido,correo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        nombre=(TextView)findViewById(R.id.nombreperfil);
+        apellido=(TextView)findViewById(R.id.apellidoperfil);
+        correo=(TextView)findViewById(R.id.correoperfil);
     }
     public boolean onCreateOptionsMenu(Menu m){
         getMenuInflater().inflate(R.menu.menu,m);
