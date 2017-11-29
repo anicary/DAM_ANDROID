c5b51498ebbe495d8c57e6c0ce36ddf8ad5851a3
ancamondragon <carobox96@gmail.com>
2017-11-28 17:45:10 -0700

    perfil

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
@@ -1,7 +1,10 @@
 package mx.edu.ittepic.dadm_proyectofinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Perfil extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+    }
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.menu,m);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem mi){
+        switch (mi.getItemId()){
+            case R.id.editar:
+
+                break;
+            case R.id.cerrarse:
+
+                break;
+        }
+        return true;
     }
 }
