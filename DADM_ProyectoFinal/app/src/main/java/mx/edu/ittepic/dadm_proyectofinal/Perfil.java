8c93b5655d3a7ba7ca7c4b41d03099bb7d619746
fernando <feranimaciones@gmail.com>
2017-11-30 12:26:59 -0700

    act

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Perfil.java
@@ -3,14 +3,21 @@ package mx.edu.ittepic.dadm_proyectofinal;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+
 public class Perfil extends AppCompatActivity {
-    String nombrea="",apellidosa="",correoa="";
+    String nombrea="",apellidosa="",correoa="",imagen="";
     TextView nombre,apellido,correo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,12 @@ public class Perfil extends AppCompatActivity {
         nombrea = prefs.getString("nombre", "Nombre");
         apellidosa = prefs.getString("apellidos", "apellidos");
         correoa = prefs.getString("correo", "correo@email.com");
+        imagen = prefs.getString("imagen", "http://carolina.x10host.com/archivos/fotos/perfil.jpg");
         nombre.setText(nombrea);
         apellido.setText(apellidosa);
         correo.setText(correoa);
+        new Perfil.DescargarImagenes((ImageView) v.findViewById(R.id.imgPer))
+                .execute(""+imagen);
     }
     public boolean onCreateOptionsMenu(Menu m){
         getMenuInflater().inflate(R.menu.menu,m);
@@ -46,5 +56,29 @@ public class Perfil extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+    private class DescargarImagenes extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DescargarImagenes(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
