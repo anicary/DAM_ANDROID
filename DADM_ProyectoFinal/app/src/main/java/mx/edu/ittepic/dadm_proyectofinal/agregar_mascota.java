c34c0159f4a97aca28389eba02029c0d1f5d9f28
fernando mac <feranimaciones@gmail.com>
2017-12-01 13:20:29 -0700

    act

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/agregar_mascota.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/agregar_mascota.java
@@ -1,31 +1,48 @@
 package mx.edu.ittepic.dadm_proyectofinal;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import static android.R.attr.bitmap;
 
 public class agregar_mascota extends AppCompatActivity implements AsyncResponse {
     EditText nombre, edad;
     String name, sex, edad1,idusuarios="";;
     Spinner tipo, raza, sexo;
-    Button agregar;
+    Button agregar,galerria,camara;
     ConexionWeb conexionWeb;
-
+    private static int RESULT_LOAD_IMAGE = 1;
+    ImageView imageView;
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Registra a tu mascota");
+        ActivityCompat.requestPermissions(this,
+                new String[]{ Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                1);
         setContentView(R.layout.activity_agregar_mascota);
         nombre = (EditText) findViewById(R.id.nombremascota);
         edad = (EditText) findViewById(R.id.edadmascota);
@@ -33,6 +50,7 @@ public class agregar_mascota extends AppCompatActivity implements AsyncResponse 
         tipo = (Spinner) findViewById(R.id.tipomascota);
         raza = (Spinner) findViewById(R.id.razamascota);
         agregar = (Button) findViewById(R.id.agregar);
+         imageView = (ImageView) findViewById(R.id.fotomascota1);
         SharedPreferences prefs = getSharedPreferences("INFO_USUARIO", Context.MODE_PRIVATE);
         idusuarios = prefs.getString("idusuarios", "0");
         agregar.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +81,71 @@ public class agregar_mascota extends AppCompatActivity implements AsyncResponse 
                 }
             }
         });
+        galerria = (Button) findViewById(R.id.btnGaleria);
+        galerria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+        camara = (Button) findViewById(R.id.btnFotodog);
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 7);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap bmp = null;
+            try {
+                bmp = getBitmapFromUri(selectedImage);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            imageView.setImageBitmap(bmp);
+        }
+        if (requestCode == 7 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
     }
     @Override
     public void procesarRespuesta(String r){}
