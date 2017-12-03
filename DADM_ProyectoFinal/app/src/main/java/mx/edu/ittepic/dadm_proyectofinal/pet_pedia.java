6777188454433b2193ee6f1f5f37cf4f1af5ab3c
ancamondragon <carobox96@gmail.com>
2017-12-02 21:10:18 -0700

    petpedia

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/pet_pedia.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/pet_pedia.java
@@ -1,13 +1,60 @@
 package mx.edu.ittepic.dadm_proyectofinal;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class pet_pedia extends AppCompatActivity {
+    razaAdaptador adapter;
+    BDInterna bdInterna;
+    ArrayList<raza> elemento;
+    ArrayList<raza> elementos;
+    ConexionWeb conexionWeb;
+    JSONArray arrayjson;
+    ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_pedia);
+        lista = (ListView) findViewById(R.id.lista);
+        cargarRazas();
+
+    }
+
+    private class DescargarImagenes extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DescargarImagenes(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+    }
+    public void cargarRazas(){
+
     }
 }
+
