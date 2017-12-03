04409eab086a3035ee0e4a29286fca03bbae3668
ancamondragon <carobox96@gmail.com>
2017-12-02 22:56:49 -0700

    mañana

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Pet_pedia_info.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/Pet_pedia_info.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Pet_pedia_info extends AppCompatActivity {
 
@@ -15,6 +16,15 @@ public class Pet_pedia_info extends AppCompatActivity {
         setContentView(R.layout.activity_pet_pedia_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+       String caracter = getIntent().getExtras().getString("caracter");
+        String salud =getIntent().getExtras().getString("salud");
+       String caract= getIntent().getExtras().getString("caracteristicas");
+       String utlidad=getIntent().getExtras().getString("utilidad");
+        String foto=getIntent().getExtras().getString("foto");
+        
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
