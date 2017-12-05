dbf3261f626d1ba045a51df802d1b3318effbd87
fernando <feranimaciones@gmail.com>
2017-12-05 12:10:06 -0700

    act

--- DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/tinderpet.java
+++ DADM_ProyectoFinal/app/src/main/java/mx/edu/ittepic/dadm_proyectofinal/tinderpet.java
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.rahuljanagouda.statusstories.StatusStoriesActivity;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -24,11 +26,12 @@ public class tinderpet extends AppCompatActivity implements  AsyncResponse {
     ListView Menu_lista;
     ConexionWeb conexionWeb;
     String idusuarios="";
+    String imagenesMostrar[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tinderpet);
-        setTitle("Tinder Pet");
+      //  setTitle("Tinder Pet");
         SharedPreferences prefs =  getSharedPreferences("INFO_USUARIO", Context.MODE_PRIVATE);
         idusuarios = prefs.getString("idusuarios", "0");
         Menu_lista = (ListView) findViewById(R.id.tinderlista);
@@ -41,7 +44,25 @@ public class tinderpet extends AppCompatActivity implements  AsyncResponse {
     public boolean onOptionsItemSelected(MenuItem mi){
         switch (mi.getItemId()){
             case R.id.pet:
+                final String[] resources = new String[]{
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00001.jpg?alt=media&token=460667e4-e084-4dc5-b873-eefa028cec32",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00002.jpg?alt=media&token=e8e86192-eb5d-4e99-b1a8-f00debcdc016",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00004.jpg?alt=media&token=af71cbf5-4be3-4f8a-8a2b-2994bce38377",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00005.jpg?alt=media&token=7d179938-c419-44f4-b965-1993858d6e71",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00006.jpg?alt=media&token=cdd14cf5-6ed0-4fb7-95f5-74618528a48b",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00007.jpg?alt=media&token=98524820-6d7c-4fb4-89b1-65301e1d6053",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00008.jpg?alt=media&token=7ef9ed49-3221-4d49-8fb4-2c79e5dab333",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00009.jpg?alt=media&token=00d56a11-7a92-4998-a05a-e1dd77b02fe4",
+                        "https://firebasestorage.googleapis.com/v0/b/firebase-satya.appspot.com/o/images%2Fi00010.jpg?alt=media&token=24f8f091-acb9-432a-ae0f-7e6227d18803",
+                };
 
+                Intent a = new Intent(tinderpet.this, StatusStoriesActivity.class);
+                a.putExtra(StatusStoriesActivity.STATUS_RESOURCES_KEY, resources);
+                a.putExtra(StatusStoriesActivity.STATUS_DURATION_KEY, 3000L);
+                a.putExtra(StatusStoriesActivity.IS_IMMERSIVE_KEY, true);
+                a.putExtra(StatusStoriesActivity.IS_CACHING_ENABLED_KEY, true);
+                a.putExtra(StatusStoriesActivity.IS_TEXT_PROGRESS_ENABLED_KEY, true);
+                startActivity(a);
                 break;
         }
         return true;
