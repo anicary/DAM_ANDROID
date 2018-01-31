30c7b75973c18befa3a46dddf0ade6029fbd1e8f
name <carobox96@gmail.com>
2018-01-31 10:55:44 -0700

    .

--- TDM_U1/app/src/main/java/mx/edu/ittepic/tdm_anacarolinamondragonrangel_calculadora/MainActivity.java
+++ TDM_U1/app/src/main/java/mx/edu/ittepic/tdm_anacarolinamondragonrangel_calculadora/MainActivity.java
@@ -2,12 +2,18 @@ package mx.edu.ittepic.tdm_anacarolinamondragonrangel_calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView result;
     Button borrar,uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve,cero,sumar,restar,dividir,mult,pto,igual;
+    double num1,num2;
+    double res;
+    String n1="";
+    int cont=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,196 @@ public class MainActivity extends AppCompatActivity {
         ocho = findViewById(R.id.num8);
         nueve = findViewById(R.id.num9);
         cero = findViewById(R.id.numcero);
-        sumar =findViewById(R.id.suma);
+        sumar = findViewById(R.id.suma);
         restar = findViewById(R.id.resta);
         dividir = findViewById(R.id.dividir);
         mult = findViewById(R.id.mult);
         pto = findViewById(R.id.punto);
         igual = findViewById(R.id.igual);
+
+        cero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "0";
+                result.setText(n1);
+            }
+        });
+        uno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "1";
+                result.setText(n1);
+            }
+        });
+        dos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "2";
+                result.setText(n1);
+            }
+        });
+        tres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "3";
+                result.setText(n1);
+            }
+        });
+        cuatro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "4";
+                result.setText(n1);
+            }
+        });
+        cinco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "5";
+                result.setText(n1);
+            }
+        });
+        seis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "6";
+                result.setText(n1);
+            }
+        });
+        siete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "7";
+                result.setText(n1);
+            }
+        });
+        ocho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "8";
+                result.setText(n1);
+            }
+        });
+        nueve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = n1 + "9";
+                result.setText(n1);
+            }
+        });
+        sumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try{
+
+                    String temporal = result.getText().toString();
+                    num1= Double.parseDouble(temporal);
+
+
+                }catch (NumberFormatException e){
+
+                    Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                cont=1;
+                result.setText("");
+                n1="";
+            }
+        });
+        restar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+
+                    String temporal = result.getText().toString();
+                    num1= Double.parseDouble(temporal);
+
+                }catch (NumberFormatException e){
+
+                    Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                cont=2;
+                result.setText("");
+                n1="";
+            }
+        });
+
+        mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+
+                    String temporal = result.getText().toString();
+                    num1= Double.parseDouble(temporal);
+
+                }catch (NumberFormatException e){
+
+                    Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                cont=3;
+                result.setText("");
+                n1="";
+            }
+        });
+        dividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+
+                    String temporal = result.getText().toString();
+                    num1= Double.parseDouble(temporal);
+
+                }catch (NumberFormatException e){
+
+                    Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                cont=4;
+               result.setText("");
+                n1="";
+            }
+        });
+       igual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temporal = result.getText().toString();
+                num2=Double.parseDouble(temporal);
+
+                switch (cont){
+                    case 1:
+                        res=num1+num2;
+                        break;
+                    case 2:
+                        res=num1-num2;
+                        break;
+                    case 3:
+                        res=num1*num2;
+                        break;
+                    case 4:
+                        res=num1/num2;
+                        break;
+                }
+
+               result.setText(""+res);
+
+
+            }
+        });
+        borrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("");
+                num1=0;
+                num2=0;
+                n1="";
+                res=0;
+            }
+        });
+
     }
+
 }
+
